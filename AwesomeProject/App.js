8c4eec94ce708e633bfed91fde3c6ee3f1dbcb5b
Humberto <betoveenmorales@gmail.com>
2023-08-24 09:01:34 -0600

    Primer ReactNative

---
+++ AwesomeProject/App.js
@@ -0,0 +1,24 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+
+//Unidad1Práctica1
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Cambio uno</Text>
+      <Text>Cambio dos</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f000ff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
