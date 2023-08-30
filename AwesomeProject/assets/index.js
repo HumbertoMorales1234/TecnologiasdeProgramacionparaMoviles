d5289bba4caf77216e8b17a381e0c7cf03d257d6
Humberto <betoveenmorales@gmail.com>
2023-08-30 09:21:49 -0600

    Errores corregidos :)

--- AwesomeProject/assets/index.js
+++ AwesomeProject/assets/index.js
@@ -1,2 +1,2 @@
-export {default as googleImage} from require('./Google.png')
-export {default as facebookImage} from require('./Facebook.png')
+export {default as googleImage} from './Google.png'
+export {default as facebookImage} from './Facebook.png'
