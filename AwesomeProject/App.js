f1aeba3a0057ffdf34a4be81e7361c82dcffaefa
Humberto <betoveenmorales@gmail.com>
2023-09-05 09:02:13 -0600

    Practica 6.5

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,56 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { FlatList, StyleSheet, Text, View } from 'react-native';
-import StyledFlatlist from './src/components/StyledFlatlist'
+import { StyleSheet, Text, View, StatusBar as ReactStatus, SafeAreaView, Platform} from 'react-native';
+//* SafeAreaView: Solo funciona en IOS, la statusbar de react-native solo funciona en android
+//
+import Constants from 'expo-constants'; // npx expo install expo-constants
+//? Provee componentes compatibles con Android y IOS
+//import { BsFillBellFill } from 'react-icons'; //! Solo funciona en React
+import {Ionicons} from '@expo/vector-icons' //? iconos de expo
+import { IosText } from './IosText';
+import { WebText } from './WebText';
 
-const DATA=[
-  {id: 0, name: 'Joe', lastname: 'Deckard'},
-  {id: 1, name: 'Beto', lastname: 'Morales'},
-  {id: 2, name: 'Gustavo', lastname: 'Avila'},
-  {id: 3, name: 'Maryjose', lastname: 'Contreras'},
-  {id: 4, name: 'Ismael', lastname: 'Diaz'},
-  {id: 5, name: 'Pau', lastname: 'Crespo'},
-  {id: 6, name: 'Teresa', lastname: 'Rivera'},
-  {id: 7, name: 'Marco', lastname: 'Solis'},
-  {id: 8, name: 'Sebastian', lastname: 'Castro'},
-  {id: 9, name: 'Luis', lastname: 'Reynaldo'},
-  {id: 10, name: 'Kratos', lastname: 'Nose'},
-  {id: 11, name: 'José', lastname: 'Madero'},
-  {id: 12, name: 'Miguel', lastname: 'Hidalgo'},
-]
 
-//Flatlist permite renderizar varios elementos a manera de scroll
+const CustomText = Platform.select({
+  android: () => IosText,
+  Web: () => WebText,
+})();
+
 export default function App() {
   return (
     <View style={styles.container}>
-      {/*<Login/> */}
-      <FlatList data={DATA} style={styles.lista} 
-      renderItem={({item: {name, lastname}, index})=>
-        <StyledFlatlist name={name} lastname={lastname} index={index}/>
-      }
-      ItemSeparatorComponent={<View style={styles.separador}></View>}
-      keyExtractor={({id}) => id}
-      />
-      <StatusBar style='auto'/>
+      <View style={styles.header}>
+          <Text>Wallet</Text>
+          <View>
+              <Ionicons name='notifications' size={50} color='#F0F'/>
+              <Ionicons name='person-circle' size={50} color='#F0F'/>      
+          </View>
+      </View>
+      <CustomText/>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingTop:50,
-    paddingHorizontal:8
+    flex: 1,
+    //backgroundColor: Platform.OS === 'web' ? '#000000': '#FFFFFF',
+    backgroundColor: Platform.select({
+      android:'green',
+      ios: 'blue',
+      web: 'black',
+    }),
+    paddingTop: Constants.statusBarHeight, // *Funciona en IOS y Android
+    //? paddingTop: ReactStatus.currentHeight, // Este componente solo funciona en android
+    ...Platform.select({
+      ios:{
+        backgroundColor: 'cyan',
+        paddingTop: 20,
+      },
+      web:{
+        backgroundColor: 'yellow'
+      }
+    }),
   },
-  lista: {
-    borderWidth:1,
-    borderColor: '#808080',
-    borderRadius: 20,
-    margin: 5,
-    padding: 1,
-
-  },
-  separador:{
-    borderWidth:1, 
-    borderColor: '#808080',
-    marginHorizontal: 3,
+  header: {
+    flexDirection: 'row',
+    alignItems:'center',
+    justifyContent:'space-between',
   },
 });
---
+++ AwesomeProject/IosText.jsx
@@ -0,0 +1,13 @@
+import { Text, View } from "react-native";
+import React from "react";
+
+export const IosText = ()=>{
+    return(
+        <View>
+            <Text>IOS</Text>
+            <Text>IOS</Text>
+            <Text>IOS</Text>
+            <Text>IOS</Text>
+        </View>
+    )
+}
