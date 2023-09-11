1548e9b1f115738d104dcefd4c33eb9539bc821d
Humberto <betoveenmorales@gmail.com>
2023-09-11 09:01:17 -0600

    Practica 10

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,23 +1,50 @@
-import React from 'react';
-import 'react-native-gesture-handler';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { useState } from 'react'; // Baja solo el hook useState
+//import React from 'react'; //Bajar todo lo que tiene react
 
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+//Hooks
 
-import WelcomeScreen from './src/screens/WelcomeScreen';
-import SignInScreen from './src/screens/SignInScreen';
-import SignUpScreen from './src/screens/SignUpScreen';
-
-const Stack = createStackNavigator();
+//Custom hooks
 
 export default function App() {
+
+  //const state = useState(1) //Pasar parametros aqui es pasar el valor del primer estado
+  const [state, setSatate] = useState(1)
+  const [inputValue, setInputValue] = useState('Value')
+
+  //console.log(state[0], '----State 0----')
+  //*console.log(state, '----State 0----')
+  //console.log(state[1], '----State 1----') //En posición uno siempre hay una función
+  //*console.log(setSatate, '----State 1----') //En posición uno siempre hay una función
+
+  const sumValue= ()=>{
+    setSatate(state+1) // State por si mismo no es modificable, es necesario usar una función adicional
+  }
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="WelcomeScreen" component={WelcomeScreen}  options={{headerShown:false}} />
-        <Stack.Screen name="SignInScreen"  component={SignInScreen}   options={{headerShown:false}} />
-        <Stack.Screen name="SignUpScreen"  component={SignUpScreen}   options={{headerShown:false}} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    
+      <View style={styles.container}>
+        {/* <Text>{state}</Text> */}
+        {/* <Text>{JSON.stringify(state)}</Text> Parsear objetos a string para poder imprimirlos */}
+        {/* <TouchableOpacity style={{borderWidth:1, padding: 5}} */}
+        {/* onPress={sumValue}> */}
+        {/* onPress={() =>sumValue()}Para poder usar parametros */}
+          {/* <Text>Change Value</Text> */}
+        {/* </TouchableOpacity> */}
+        <TextInput value={inputValue}
+        onChangeText={(value) => setInputValue(value)}/>
+        <Text>Value: {inputValue}</Text>
+        <StatusBar style="auto" />
+      </View>
+    
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
