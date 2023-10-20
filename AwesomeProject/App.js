82e7deb543900e15dd46de2494a549fd5f84bc78
Humberto <betoveenmorales@gmail.com>
2023-10-20 16:20:20 -0600

    Practica 2

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,26 +1,25 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View} from 'react-native';
-import { Card } from './src/components/card';
-import { AppContextProvider } from './src/context/AppContext';
-import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, Text, View } from 'react-native';
+import ResumenScreen from './src/screens/ResumenScreen';
+import BaseScreen from './src/screens/BaseScreen';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 import { NavigationContainer } from '@react-navigation/native';
-import { LoginScreen } from './src/screens/LoginScreen';
-import { DetailsScreen } from './src/screens/DetailsScreen';
-import { AcountScreen } from './src/screens/AcountScreen';
-import { AuthContextProvider } from './src/context/AuchContex';
-import { Layout } from './src/Layout/Layout';
 
-// npm install @react-navigation/native
-// npx expo install react-native-screens react-native-safe-area-context
-// npm install @react-navigation/stack
-// npx expo install react-native-gesture-handler
+
+const Tab = createMaterialTopTabNavigator()
+
+//npx expo install expo-file-system
+//npx expo install expo-media-library
+//npx expo install expo-image-picker
 
 export default function App() {
-
   return (
-      <AuthContextProvider>
-        <Layout/>
-      </AuthContextProvider>
+    <NavigationContainer>
+    <Tab.Navigator screenOptions={{tabBarStyle:{paddingTop: 40}}}>
+      <Tab.Screen name='Base' component={BaseScreen}/>
+      <Tab.Screen name='Resume' component={ResumenScreen}/>
+    </Tab.Navigator>
+  </NavigationContainer>
   );
 }
 
--- AwesomeProject/src/hooks/useAuthContext.js
+++
@@ -1,4 +0,0 @@
-import { useContext } from "react";
-import { AuthContext } from "../context/AuchContex";
-
-export const useAuthContext = () => useContext(AuthContext)
